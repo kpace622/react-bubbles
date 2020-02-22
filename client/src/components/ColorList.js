1fe1f14b5849ae24cc8cdc87082a2626808b6676
Kyle Pace <kpace2296@gmail.com>
2020-02-21 22:11:37 -0600

    finished mvp

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from 'react-router-dom';
 
 const initialColor = {
   color: "",
@@ -10,6 +12,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  let history = useHistory();
 
   const editColor = color => {
     setEditing(true);
@@ -21,10 +24,27 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors([
+          ...colors.filter(color => color.id !== colorToEdit.id),
+          res.data
+        ])
+        setEditing(false)
+      })
+      .catch(err => console.error(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/colors/${color.id}`)
+    .then(() => {
+      updateColors(colors.filter(color => colors.id !== color.id));
+      history.push('/')
+    })
+    .catch(err => console.log(err));
   };
 
   return (
