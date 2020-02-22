6e9196790c5f79291baec93d31a21b26bd4e57f1
Kyle Pace <kpace2296@gmail.com>
2020-02-21 21:21:47 -0600

    working on stage 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import ProtectedRoute from './components/ProtectedRoute';
+import BubblePage from './components/BubblePage';
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -8,11 +10,23 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <ul>
+          <li>
+            <Link to='/'>Login</Link>
+          </li>
+          <li>
+            <Link to='/bubble-page'>Bubble Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <ProtectedRoute exact path='/bubble-page' component={BubblePage} />
+          <Route exact path="/" component={Login} />
+          <Route component={Login} />
+        </Switch>
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [username, setUsername] = useState('Lambda School');
+  const [password, setPassword] = useState('i<3Lambd4');
+
+  let history = useHistory();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/login', {username, password})
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token', res.data.payload);
+        history.push('/bubble-page')
+      })
+      .catch(err => {
+        localStorage.removeItem('token');
+        console.error('invalid', err)
+      })
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button>Log in</button>
+      </form>
     </>
   );
 };
